33f6341a2976aa9f56d400d4597e3d6c2696ed8f
Crocodile <wwwblogger1@gmail.com>
2022-09-30 21:16:25 +0300

    add Program.cs

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+
+namespace Benchmark
+{
+    /*
+     * Boxing and unboxing
+     */
+    // добавляя упаковку и разпаковку, добавляем время на это и ктому-же с временем нужна будет работа габидж коллектора
+
+    [MemoryDiagnoser]
+
+    public class BoxingTest
+    {
+
+        [Benchmark]
+
+        public void NoBoxing()
+        {
+            int res = 0;
+            int a = 1;
+            res += a;
+        }
+        [Benchmark]
+
+        public void Boxing()
+        {
+            int res = 0;
+            object a = 1;
+            res = +(int)a;
+        }
+    }
+
+    class Program
+    {
+        
+        public static void Main(string[] args)
+        {
+            int a = 1; 
+            object b = a; 
+            int c = (int)b; 
+
+            //decimal d = (decimal)b; // InvalidCastException // распаковать 1 как decimal, не получится - будет ошибка.
+// add new file
+        }
+    }
+}
